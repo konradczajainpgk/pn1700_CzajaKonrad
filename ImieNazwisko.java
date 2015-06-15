44297462fdd0dc5ca043b278b426cf14becf1f46
cvcv <cxvxcv@sfds.pl>
2015-06-15 18:14:57 +0200

    "To ja"

---
+++ ImieNazwisko.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imienazwisko;
+
+/**
+ *
+ * @author student215
+ */
+public class ImieNazwisko {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Konrad Czaja!"); // Display the string.
+    }
+    
+}
